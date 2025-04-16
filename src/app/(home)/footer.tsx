2fcc998110c32bbadaa8d01b51fb5477533fa329
devansh <devanshverma193@gmail.com>
2025-04-16 11:51:57 +0530

    home-layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div className="flex item-center gap-3">
+        <p>Silk route</p>
+      </div>
+    </footer>
+  );
+};
