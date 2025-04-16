585601df4f296193b160fa6beb8a4db4989e2c31
devansh <devanshverma193@gmail.com>
2025-04-16 17:46:34 +0530

    search-filters

---
+++ src/app/(app)/(home)/search-filter/categories.tsx
@@ -0,0 +1,25 @@
+import { Category } from "@/payload-types";
+import { CategoryDropDown } from "./category-dropdown";
+
+interface Props {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
+}
+
+export const Categories = ({ data }: Props) => {
+  return (
+    <div className="relative w-full">
+      <div className=" flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div key={category.id}>
+            <CategoryDropDown
+              category={category}
+              isActive={false}
+              isNavigationHovered={false}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
