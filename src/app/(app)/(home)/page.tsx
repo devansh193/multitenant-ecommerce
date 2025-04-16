7d622ec727a2ca6a27e649ec222e45ec4ceec249
devansh <devanshverma193@gmail.com>
2025-04-16 13:24:42 +0530

    payload-integration

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,16 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  const data = await payload.find({
+    collection: "categories",
+  });
+  return (
+    <div className="flex flex-col gap-y-4 m-4">
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
+}
